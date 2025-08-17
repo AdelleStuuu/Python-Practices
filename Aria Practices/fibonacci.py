68cef3759df52da41b2e783cd376bbda96b1354a
AdelleStuuu <adelledoesstuff@gmail.com>
2025-08-18 01:11:48 +0800

    fibonacci sequence

---
+++ Aria Practices/fibonacci.py
@@ -0,0 +1,28 @@
+print()
+iter = int(input('input iterations for fibbonacci sequence: '))
+leftNumber = 0
+rightNumber = 0
+currentNumber = 0
+i = 0
+fibonacci = ''
+
+while i != iter:
+    if i == 0:
+        fibonacci += '0,'
+    elif i == 1:
+        fibonacci += '1,'
+        rightNumber = 1
+    else: 
+        currentNumber = leftNumber + rightNumber
+        fibonacci += str(currentNumber)
+        leftNumber = rightNumber
+        rightNumber = currentNumber
+        if i == iter - 1:
+            fibonacci += '.'
+        else:
+            fibonacci += ','
+    i += 1
+    
+
+print('the sequence is:',fibonacci) 
+print()

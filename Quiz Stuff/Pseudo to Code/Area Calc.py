d49c3a068323ef5f269fb0c28160ba3d132dda4f
AdelleStuuu <adelledoesstuff@gmail.com>
2025-08-18 08:55:14 +0800

    pesudo to code part 1

---
+++ Quiz Stuff/Pseudo to Code/Area Calc.py
@@ -0,0 +1,4 @@
+length = int(input('Enter value of Length: '))
+width = int(input('Enter value of Width: '))
+area = length * width
+print('The area is:',area )
---
+++ Quiz Stuff/Pseudo to Code/Miles to Kilometer.py
@@ -0,0 +1,3 @@
+miles = int(input('Enter the amount of miles: '))
+kilometer = float(miles) * 1.609 
+print(miles,'in miles to Kilometer is:', kilometer)

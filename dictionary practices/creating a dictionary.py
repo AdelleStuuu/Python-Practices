9f92f820cae2719457f826e9534722290d733f35
AdelleStuuu <adelledoesstuff@gmail.com>
2025-09-11 23:28:18 +0800

    simple dictionary in python

---
+++ dictionary practices/creating a dictionary.py
@@ -0,0 +1,12 @@
+personalInfo = {
+    "name": "Adelle Meitner",
+    "age": 30,
+    "city": "New York",
+    "occupation": "Software Engineer"
+}
+
+# Printing the entire dictionary 
+print(personalInfo)
+
+# Accessing values
+print("Name:", personalInfo["name"])

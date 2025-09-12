880ddaa3a3908ab3721221a9228eebc0fd77fce0
AdelleStuuu <adelledoesstuff@gmail.com>
2025-09-12 16:59:36 +0800

    a program that adds all the values of a dictionary to display the sum

---
+++ dictionary practices/sum of all values.py
@@ -0,0 +1,12 @@
+print()
+numbers = {
+    'num1': 10,
+    'num2': 20,
+    'num3': 30,
+    'num4': 40
+}
+
+sum = numbers['num1'] + numbers['num2'] + numbers['num3'] + numbers['num4']
+
+print(f'The sum of all values in the dictionary is: {sum}')
+print()

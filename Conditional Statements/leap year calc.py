fc41756ff24987bca704491f560b4f494da8e980
AdelleStuuu <adelledoesstuff@gmail.com>
2025-09-12 18:27:05 +0800

    a program that checks if it is a leap year or not

---
+++ Conditional Statements/leap year calc.py
@@ -0,0 +1,16 @@
+print()
+while True:
+    try:
+        year = int(input("Enter a year: "))
+    except ValueError:
+        print('Enter an integer value.')
+        print()
+        continue
+    
+    break
+
+if year % 4 == 0: 
+    print('It is a leap year!')
+else:
+    print('oop, not a leap year...')
+print()

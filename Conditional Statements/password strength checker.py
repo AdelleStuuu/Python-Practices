b0ed69f108f57b7a962c0a8c5a2ae66a4d82f4d3
AdelleStuuu <adelledoesstuff@gmail.com>
2025-09-12 20:51:37 +0800

    a program that checks if the password you have is strong or weak

---
+++ Conditional Statements/password strength checker.py
@@ -0,0 +1,30 @@
+print()
+password = input('Enter your password here!: ')
+passwordSliced = list(password)
+numbers = str([1,2,3,4,5,6,7,8,9,0])
+strengthPoints = 0
+print()
+for bigLetters in passwordSliced:
+    if bigLetters.isupper():
+        strengthPoints += 1
+
+for numChecking in passwordSliced:
+    if numChecking in numbers:
+        strengthPoints += 2 
+
+for specialCharacters in passwordSliced:
+    if not(specialCharacters.isalpha() or specialCharacters.isdigit()):
+        strengthPoints += 3
+        
+if len(password) >= 12:
+    strengthPoints += 10
+elif len(password) >= 8:
+    strengthPoints += 5
+
+if strengthPoints > 15:
+    print('Ooo, strong password!')
+elif strengthPoints > 10:
+    print('Hmm, its an okay password!')
+else:
+    print('oomf, that password kinda weak!')
+print()

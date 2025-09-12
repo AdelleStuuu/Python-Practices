54a95275d60ef82e847402e882e759ad391379ef
AdelleStuuu <adelledoesstuff@gmail.com>
2025-09-12 18:15:31 +0800

    added error handling to odd or even

--- Conditional Statements/odd or even again.py
+++ Conditional Statements/odd or even again.py
@@ -1,6 +1,16 @@
-number = int(input("Enter a number: "))
+print()
+while True:
+    try:
+        number = int(input("Enter a number: "))
+    except ValueError:
+        print('Enter an integer value.')
+        print()
+        continue
+    
+    break
 
 if number % 2 == 0: 
     print(f"{number} is even.")
 else:
     print(f"{number} is odd.")
+print()

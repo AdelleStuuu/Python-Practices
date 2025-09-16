3ee0511e9beb8cd876ffa26052bb32f2ae346a13
AdelleStuuu~ <adelledoesstuff@gmail.com>
2025-09-16 23:52:14 +0800

    a program that counts the frequency of words in a given sentence

---
+++ dictionary practices/word counter.py
@@ -0,0 +1,13 @@
+print()
+sentence =  input("Enter a sentence: ")
+words = sentence.split()
+word_count = {}
+
+for word in words: 
+    if word in word_count:
+        word_count[word] += 1
+    else:
+        word_count[word] = 1
+        
+print()
+print(word_count)

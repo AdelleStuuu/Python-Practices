4cb8e1c1e45af20d60762e0f5e07af1a708d152e
AdelleStuuu~ <adelledoesstuff@gmail.com>
2025-09-17 00:57:41 +0800

    a program that checks a student database with their name and grade, update their grades as well as add or remove students

--- dictionary practices/student grades.py
+++ dictionary practices/student grades.py
@@ -3,20 +3,32 @@ print("Welcome to Adelle\'s grade lister !!!")
 studentList = {
     'Katherina' : 95,
     'Orion' : 80,
-    'Caeled' : 85,
+    'Caeled' : 90,
     'Alter' : 75,
-    'Sophie' : 90,
+    'Sophie' : 80,
 }
 action = 0
+def line():
+    print("+--------------------------------+")
 
 while True:
+    print()
+    line()
+    print()
     try:
-        action = int("Input an action provided!\n1. Check grades\n2. Update Grades\n3. Add or Remove Students!")
+
+        action = int(input("Input an action provided!\n1. Check Database!!\n2. Update Grades~\n3. Add or Remove Students!\n4. Quit the Program\nEnter Answer: "))
+
     except ValueError:
-        print("Input an integer.")
+        print("wOAH? Thats not an Integer?")
+
     else: 
-        if action == 1 or action == 2 or action == 3:
+        if action == 1 or action == 2 or action == 3 or action == 4:
+            print()
             print("I see, good choice!")
+            print()
+            line()
+            print()
             if action == 1:
                 print("Okie dokie, here are the student List!")
                 for student,grade in studentList.items():
@@ -30,6 +42,83 @@ while True:
                         gradeLetter = 'D'
                     else:
                         gradeLetter = 'F'
-                    print(f"Student Name {student}, Grade: {gradeLetter}")
+                    print(f"Name: \"{student}\", Grade: {gradeLetter}")
+
+            elif action == 2: 
+                print("Updating Grades!")
+                while True:
+                    print()
+                    studentName = input("Enter the Student's name! (its case sensitive!): ")
+                    if studentName in studentList:
+                        print(f"Okay! Found {studentName}! What to do now?")
+                        while True:
+                            try:
+                                newGrade =  int(input("Enter Grade here: "))
+                            except ValueError:
+                                print("Silly billy, Grades are Integers!")
+                            else:
+                                studentList[studentName] = newGrade
+                                break
+                        print("Student Grade is updated!")
+                        break
+                    else:
+                        print("Nope! Student is not here!")
+
+            elif action == 3:
+                while True:
+                    print()
+                    try:
+                        addOrRemove = int(input("Well, Whatcha wanna do?\n1. Add a student\n2. Remove a student\nEnter Choice: "))
+                    except ValueError:
+                        print("Hmm, That doesn't seem like an integer?")
+                    else:
+                        print()
+                        if addOrRemove == 1:
+                            print("Great! A new Student!")
+                            while True:
+                                newStudentName = input("Enter their name here!: ")
+                                studentNameSpliced = list(newStudentName)
+                                for isLetter in studentNameSpliced:
+                                    if isLetter.isdigit():
+                                        isThereNumbers = True
+                                        break
+                                    else:
+                                        isThereNumbers = False
+                                if isThereNumbers == True:
+                                    print("Hm, Why does the name have numbers?")
+                                else:
+                                    break
+                            
+                            while True:
+                                try:
+                                    newStudentGrade = int(input("Enter their grade here!: "))
+                                except ValueError:
+                                    print("Hi! That not an Integer!")
+                                else:
+                                    break
+                            
+                            studentList[newStudentName] = newStudentGrade
+                            break
+
+                        elif addOrRemove == 2:
+                            print("Welp, Bye Bye Student!")
+                            while True:
+                                existingStudentname = input("Enter student name here!: ")
+                                if existingStudentname in studentList:
+                                    del studentList[existingStudentname]
+                                    isDone = True
+                                    break
+                                else:
+                                    print("That doesn't seem like a student in our records?")
+                                    isDone = False
+                            
+                            if isDone == True:
+                                break
+                        
+            elif action == 4: 
+                print("See ya Later!")
+                print()
+                break
+
         else: 
             print("Please pick a valid number!")

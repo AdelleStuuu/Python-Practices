402d0dc6ccbab125ad0d880d6d5b1e391960dc93
AdelleStuuu~ <adelledoesstuff@gmail.com>
2025-09-19 00:36:00 +0800

    tweak that does not include zeroes

--- PRACTICES SET 3/even prints.py
+++ PRACTICES SET 3/even prints.py
@@ -3,11 +3,12 @@ numberList = []
 print()
 while number != 0:
     number = int(input("Enter yer Number! "))
-    numberList.append(number) 
+    if number != 0:
+        numberList.append(number) 
 
 print()
 for i in numberList:
-    if i % 2 == 0:
+    if i % 2 == 0 and i != 0:
         print(f"This number is even in the list {i}")
 
 print()

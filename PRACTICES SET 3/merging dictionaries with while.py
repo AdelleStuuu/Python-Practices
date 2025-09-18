fc4ce4acb9ca52f0a9816cf0700dfb0387f4dd9e
AdelleStuuu~ <adelledoesstuff@gmail.com>
2025-09-19 01:15:10 +0800

    merging dictionaries, gotta need to learn how to sort it by number though

---
+++ PRACTICES SET 3/merging dictionaries with while.py
@@ -0,0 +1,36 @@
+dictionary1 = {
+    'val 1' : 5,
+    'val 6' : 6,
+    'val 3' : 7,
+    'val 4' : 5,
+    'val 5' : 3,
+    'val 2' : 2,
+    'val 11' : 10.
+}
+
+dictionary2 = {
+    'val 9' : 5,
+    'val 7' : 6,
+    'val 3' : 7,
+    'val 8' : 5,
+    'val 10' : 3,
+    'val 2' : 2,
+}
+
+dictionary3 = {}
+
+for key,value in dictionary1.items():
+    dictionary3.update({
+        key : value, 
+    })
+for key,value in dictionary2.items():
+    if key not in dictionary3:
+        dictionary3.update({
+            key: value,
+        })
+
+dictionary3 = sorted(dictionary3.keys())
+
+print(dictionary1)
+print(dictionary2)
+print(dictionary3)

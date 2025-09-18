85f61d9503bfd2d2bb5e815f2e84e2e6a7d4fc16
AdelleStuuu~ <adelledoesstuff@gmail.com>
2025-09-19 01:22:17 +0800

    a bit of fixing

--- PRACTICES SET 3/merging dictionaries with while.py
+++ PRACTICES SET 3/merging dictionaries with while.py
@@ -1,20 +1,20 @@
 dictionary1 = {
-    'val 1' : 5,
-    'val 6' : 6,
-    'val 3' : 7,
-    'val 4' : 5,
-    'val 5' : 3,
-    'val 2' : 2,
-    'val 11' : 10.
+    'val 1 ' : 5,
+    'val 6 ' : 6,
+    'val 3 ' : 7,
+    'val 4 ' : 5,
+    'val 5 ' : 3,
+    'val 2 ' : 2,
+    'val 11' : 10
 }
 
 dictionary2 = {
-    'val 9' : 5,
-    'val 7' : 6,
-    'val 3' : 7,
-    'val 8' : 5,
+    'val 9 ' : 5,
+    'val 7 ' : 6,
+    'val 3 ' : 7,
+    'val 8 ' : 5,
     'val 10' : 3,
-    'val 2' : 2,
+    'val 2 ' : 2,
 }
 
 dictionary3 = {}
@@ -29,8 +29,11 @@ for key,value in dictionary2.items():
             key: value,
         })
 
-dictionary3 = sorted(dictionary3.keys())
-
-print(dictionary1)
-print(dictionary2)
-print(dictionary3)
+dictionary3 = sorted(dictionary3.items())
+print()
+print(f"old dictionary: {dictionary1}")
+print(f"old dictionary: {dictionary2}")
+print()
+for key,value in dictionary3:
+    print(f"key: {key}, value: {value}")
+print()

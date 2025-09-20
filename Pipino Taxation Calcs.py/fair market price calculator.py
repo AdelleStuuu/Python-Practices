5922371368b59c44d16939b4e18be07f9f1d0754
AdelleStuuu~ <adelledoesstuff@gmail.com>
2025-09-20 10:28:37 +0800

    a program that calculates the fair market value of a property in the philippines

---
+++ Pipino Taxation Calcs.py/fair market price calculator.py
@@ -0,0 +1,33 @@
+print()
+print("+--- Adelle's Market Property Value Assessment for Pipinos ---+")
+print()
+while True:
+    try:
+        taxAmount = int(input("Enter the amount you paid for taxes: "))
+    except ValueError:
+        print("Enter an integer value")
+    else:
+        break
+
+while True:
+    try:
+        yearsPaid = int(input("Enter the amount of years you paid for taxes: "))
+    except ValueError:
+        print("Enter an integer value")
+    else:
+        break
+
+while True:
+    try:
+        inflationRate = float(input("Enter the inflation rate: "))
+    except ValueError:
+        print("Enter a float value")
+    else:
+        break
+
+taxAndYearPaid = (taxAmount * yearsPaid)
+fairMarketValue = f"{(taxAndYearPaid + (taxAndYearPaid +(taxAndYearPaid * (inflationRate/100)))) * yearsPaid:,}"
+
+print()
+print(f"The fair market value is {fairMarketValue} PHP")
+print()

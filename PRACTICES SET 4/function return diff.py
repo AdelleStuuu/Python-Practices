e3c2878b4bc6690146e79fc0fdd84c450be8a4be
AdelleStuuu~ <adelledoesstuff@gmail.com>
2025-10-11 18:15:39 +0800

    a function that does subtraction

---
+++ PRACTICES SET 4/function return diff.py
@@ -0,0 +1,11 @@
+def diff(x,y):
+    difference = x - y 
+    return difference
+
+print()
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+minus = diff(num1,num2)
+
+print(f"the difference of {num1} and {num2} is {minus}\n")

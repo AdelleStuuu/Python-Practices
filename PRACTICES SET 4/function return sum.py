deddf82bffc49b05e145c2f9642786727f5432bc
AdelleStuuu~ <adelledoesstuff@gmail.com>
2025-10-11 18:12:15 +0800

    a funcation that returns the sum of a number

---
+++ PRACTICES SET 4/function return sum.py
@@ -0,0 +1,11 @@
+def sum(x,y):
+    sum = x + y 
+    return sum
+
+print()
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+add = sum(num1,num2)
+
+print(f"the sum of {num1} and {num2} is {add}\n")
